bf3669d878d756a37844725f32f97ad064394ea7
ArturVartsaba <ArturVartsaba1@gmail.com>
2025-10-09 12:18:54 +0300

    Added create get method, empty view and viewmodel, no view design

---
+++ Filminurk/Filminurk/Models/Movies/MoviesCreateViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Filminurk.Models.Movies
+{
+    public class MoviesCreateViewModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateOnly FirstPublished { get; set; }
+        public string Director { get; set; }
+        public List<string>? Actors { get; set; }
+        public double? CurrentRating { get; set; }
+        //public List<UserComment>? Reviews { get; set; }
+
+        /* 3 õpilase valitud andmetüüpi */
+        public string? Genre { get; set; }
+        public string? Language { get; set; }
+        public int? DurationInMinutes { get; set; }
+
+        /* andmebaasi jaoks vajalikud */
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
