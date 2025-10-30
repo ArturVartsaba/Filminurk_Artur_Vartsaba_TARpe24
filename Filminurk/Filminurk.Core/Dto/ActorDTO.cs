f2716feb6fa12172666afbd6688ed1f07258cdba
ArturVartsaba <ArturVartsaba1@gmail.com>
2025-10-30 09:05:44 +0200

    Created ActorDTO class

---
+++ Filminurk/Filminurk.Core/Dto/ActorDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Dto
+{
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+    public class ActorDTO
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? NickName { get; set; }
+        public List<string>? MoviesActedFor { get; set; }
+        public int? PortraitID { get; set; }
+
+        public int Age { get; set; }
+        public string CountryOfOrigin { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
