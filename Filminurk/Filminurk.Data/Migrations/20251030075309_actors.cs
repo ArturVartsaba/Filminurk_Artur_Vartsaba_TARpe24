76241bd5322d89b167379d6c17094bece18f324d
ArturVartsaba <ArturVartsaba1@gmail.com>
2025-10-30 09:59:02 +0200

    Added Index View for Actors

---
+++ Filminurk/Filminurk.Data/Migrations/20251030075309_actors.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filminurk.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class actors : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    ActorID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NickName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MoviesActedFor = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PortraitID = table.Column<int>(type: "int", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    CountryOfOrigin = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Gender = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.ActorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FilesToApi",
+                columns: table => new
+                {
+                    ImageID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExistingFilePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MovieID = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsPoster = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilesToApi", x => x.ImageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FirstPublished = table.Column<DateOnly>(type: "date", nullable: false),
+                    Director = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Actors = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CurrentRating = table.Column<double>(type: "float", nullable: true),
+                    Genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Language = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DurationInMinutes = table.Column<int>(type: "int", nullable: true),
+                    EntryCreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EntryModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserComments",
+                columns: table => new
+                {
+                    CommentID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CommenterUserID = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommentBody = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CommentedScore = table.Column<int>(type: "int", nullable: false),
+                    IsHelpful = table.Column<int>(type: "int", nullable: false),
+                    IsHarmful = table.Column<int>(type: "int", nullable: false),
+                    CommentCreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentDeletedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    MovieId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserComments", x => x.CommentID);
+                    table.ForeignKey(
+                        name: "FK_UserComments_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserComments_MovieId",
+                table: "UserComments",
+                column: "MovieId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actors");
+
+            migrationBuilder.DropTable(
+                name: "FilesToApi");
+
+            migrationBuilder.DropTable(
+                name: "UserComments");
+
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
